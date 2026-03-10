d92f6b6f9970bb132e7f8291da91242d888088c3
infofvalentim <info.fvalentim@gmail.com>
2026-03-10 18:41:17 -0300

    Create main.js to initialize application modules
    This file serves as the main entry point for the application, importing and initializing various modules on DOM content loaded.

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/**
+ * main.js
+ * Ponto de entrada da aplicação.
+ * Importa e inicializa todos os módulos.
+ */
+
+import { initMenu }        from "./menu.js";
+import { initCounters }    from "./counter.js";
+import { initForms }       from "./forms.js";
+import { initHeader, initBackToTop, initSmoothScroll, initReveal } from "./scroll.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initMenu();
+  initCounters();
+  initForms();
+  initHeader();
+  initBackToTop();
+  initSmoothScroll();
+  initReveal();
+});
